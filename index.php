4b3a6bd417b1cf29bddc252e7f768428afeac868
Tarun Chauhan <coding.tarun@gmail.com>
2022-11-26 12:44:59 +0530

    Initial setup

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- Slick Slider CDN -->
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.css" integrity="sha512-6lLUdeQ5uheMFbWm3CP271l14RsX1xtx+J5x2yeIDkkiBpeVTNhTqijME7GgRKKi6hCqovwCoBTlRBEC20M8Mg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/src/css/style.css">
+    <title>Baagicha | Do Apple Farming with sense</title>
+</head>
+<body>
+    <!--Main Navigation-->
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Dropdown
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link disabled">Disabled</a>
+                </li>
+            </ul>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+            </div>
+        </div>
+    </nav>
+    <div class="container">
+        <!-- Hero Section-->
+    <section id="hero">
+        <div class="hero_slider">
+            <div><img src="https://picsum.photos/id/237/1000/300" class="img-fluid rounded" alt="" srcset=""></div>
+            <div><img src="https://picsum.photos/id/238/1000/300" class="img-fluid rounded" alt="" srcset=""></div>
+            <div><img src="https://picsum.photos/id/239/1000/300" class="img-fluid rounded" alt="" srcset=""></div>
+        </div>
+    </section>
+    <section id="varieties">
+        <div class="varieties_slider">
+            <div><img src="https://picsum.photos/id/238/300/300" alt="" srcset="" class="img-fluid rounded"></div>
+            <div><img src="https://picsum.photos/id/237/300/300" alt="" srcset="" class="img-fluid rounded"></div>
+            <div><img src="https://picsum.photos/id/238/300/300" alt="" srcset="" class="img-fluid rounded"></div>
+            <div><img src="https://picsum.photos/id/239/300/300" alt="" srcset="" class="img-fluid rounded"></div>
+            <div><img src="https://picsum.photos/id/238/300/300" alt="" srcset="" class="img-fluid rounded"></div>
+            <div><img src="https://picsum.photos/id/240/300/300" alt="" srcset="" class="img-fluid rounded"></div>
+            <div><img src="https://picsum.photos/id/238/300/300" alt="" srcset="" class="img-fluid rounded"></div>
+            <div><img src="https://picsum.photos/id/237/300/300" alt="" srcset="" class="img-fluid rounded"></div>
+            <div><img src="https://picsum.photos/id/238/300/300" alt="" srcset="" class="img-fluid rounded"></div>
+            <div><img src="https://picsum.photos/id/239/300/300" alt="" srcset="" class="img-fluid rounded"></div>
+            <div><img src="https://picsum.photos/id/238/300/300" alt="" srcset="" class="img-fluid rounded"></div>
+            <div><img src="https://picsum.photos/id/240/300/300" alt="" srcset="" class="img-fluid rounded"></div>
+        </div>
+    </section>
+    </div>
+    
+    
+    <!-- Bootstrap Scripts CDN-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <!-- jQuery CDN -->
+    <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
+    <!-- Slick Slider JS CDN-->
+    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/slickSlider.js"></script>
+</body>
+</html>
