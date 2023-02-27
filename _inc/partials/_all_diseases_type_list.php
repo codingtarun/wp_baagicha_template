ca6d0cd96dd46dfd073efb734d7cb0abd754cab5
Tarun Chauhan <coding.tarun@gmail.com>
2023-02-27 21:10:11 +0530

    Taxonomy

--- _inc/partials/_all_diseases_type_list.php
+++ _inc/partials/_all_diseases_type_list.php
@@ -1,5 +1,5 @@
 <?php
-$diseasesTypes = get_terms(['taxonomy' => 'disease_types', 'hide_empty' => false]);
+$diseasesTypes = get_terms(['taxonomy' => 'disease_types', 'hide_empty' => false, 'orderby' => 'ID', 'order' => 'ASC', 'number' => 10]);
 //print_r($fruitsList);
 ?>
 <li class="categories">
@@ -8,7 +8,8 @@ $diseasesTypes = get_terms(['taxonomy' => 'disease_types', 'hide_empty' => false
         <?php foreach ($diseasesTypes as $type) {
         ?>
             <li class="cat-item">
-                <a href="#"><?php echo $type->name; ?></a>
+                <a href="#"> <?php echo $type->name; ?></a>
+                <small><?php echo get_wp_term_image($type->term_id); ?></small>
             </li>
         <?php } ?>
     </ul>
